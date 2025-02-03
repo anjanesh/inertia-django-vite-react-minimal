53a0702da3f86eee759daaa666b14133bc2995db
Anjanesh <anjaneshlekshminarayanan@cloudxsupport.com>
2025-02-03 09:21:02 +0530

    user count

--- app/static/src/pages/Index.tsx
+++ app/static/src/pages/Index.tsx
@@ -3,11 +3,11 @@ import { Link, usePage } from '@inertiajs/react';
 
 export default function Index() {
 
-  const { app_name } = usePage().props;
+  const { app_name, user_count } = usePage().props;
 
   return (
       <div>
-          <h1>{app_name}</h1>
+          <h1>{app_name} : {user_count} users</h1>
           <h1>Welcome to the Home Page</h1>
           <Link href="/about">About</Link>
       </div>
